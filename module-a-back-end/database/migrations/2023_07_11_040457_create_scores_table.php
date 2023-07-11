c0e06a5e850048d7e20937e16c293172ff2ae6f5
vaseles <vaselesstolarov813@gmail.com>
2023-07-11 10:23:04 +0600

    add  more

---
+++ module-a-back-end/database/migrations/2023_07_11_040457_create_scores_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constraint('users')->onDelete('cascade');
+            $table->foreignId('game_version_id')->constraint('game_versions')->onDelete('cascade');
+            $table->string('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scores');
+    }
+};
